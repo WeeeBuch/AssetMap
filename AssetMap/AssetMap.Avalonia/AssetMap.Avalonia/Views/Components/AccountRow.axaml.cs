c5071f2ff01cf4d6c9afa5c563672396bfcb5bb4
KikiZC <kikizc22@gmail.com>
2026-06-07 13:18:49 +0200

    opravy

--- AssetMap/AssetMap.Avalonia/AssetMap.Avalonia/Views/Components/AccountRow.axaml.cs
+++ AssetMap/AssetMap.Avalonia/AssetMap.Avalonia/Views/Components/AccountRow.axaml.cs
@@ -9,7 +9,7 @@ public partial class AccountRow : UserControl
     public static readonly StyledProperty<string>  IconTextProperty        = AvaloniaProperty.Register<AccountRow, string>(nameof(IconText), "?");
     public static readonly StyledProperty<IBrush>  IconBackgroundProperty  = AvaloniaProperty.Register<AccountRow, IBrush>(nameof(IconBackground), Brushes.Gray);
     public static readonly StyledProperty<IBrush>  IconForegroundProperty  = AvaloniaProperty.Register<AccountRow, IBrush>(nameof(IconForeground), Brushes.White);
-    public static readonly StyledProperty<string>  NameProperty            = AvaloniaProperty.Register<AccountRow, string>(nameof(Name), "Účet");
+    public static readonly StyledProperty<string>  AccountNameProperty     = AvaloniaProperty.Register<AccountRow, string>(nameof(AccountName), "Účet");
     public static readonly StyledProperty<string>  SubtitleProperty        = AvaloniaProperty.Register<AccountRow, string>(nameof(Subtitle), "");
     public static readonly StyledProperty<string>  ValueProperty           = AvaloniaProperty.Register<AccountRow, string>(nameof(Value), "$0");
     public static readonly StyledProperty<string>  SubValueProperty        = AvaloniaProperty.Register<AccountRow, string>(nameof(SubValue), "");
@@ -18,7 +18,7 @@ public partial class AccountRow : UserControl
     public string IconText       { get => GetValue(IconTextProperty);       set => SetValue(IconTextProperty, value); }
     public IBrush IconBackground { get => GetValue(IconBackgroundProperty); set => SetValue(IconBackgroundProperty, value); }
     public IBrush IconForeground { get => GetValue(IconForegroundProperty); set => SetValue(IconForegroundProperty, value); }
-    public string Name           { get => GetValue(NameProperty);           set => SetValue(NameProperty, value); }
+    public string AccountName    { get => GetValue(AccountNameProperty);    set => SetValue(AccountNameProperty, value); }
     public string Subtitle       { get => GetValue(SubtitleProperty);       set => SetValue(SubtitleProperty, value); }
     public string Value          { get => GetValue(ValueProperty);          set => SetValue(ValueProperty, value); }
     public string SubValue       { get => GetValue(SubValueProperty);       set => SetValue(SubValueProperty, value); }
--- AssetMap/AssetMap.Avalonia/AssetMap.Avalonia/Views/Components/TransactionRow.axaml.cs
+++ AssetMap/AssetMap.Avalonia/AssetMap.Avalonia/Views/Components/TransactionRow.axaml.cs
@@ -14,6 +14,7 @@ public partial class TransactionRow : UserControl
     public static readonly StyledProperty<string>  AmountProperty          = AvaloniaProperty.Register<TransactionRow, string>(nameof(Amount), "");
     public static readonly StyledProperty<bool>    IsPositiveProperty      = AvaloniaProperty.Register<TransactionRow, bool>(nameof(IsPositive), false);
     public static readonly StyledProperty<bool>    IsNeutralProperty       = AvaloniaProperty.Register<TransactionRow, bool>(nameof(IsNeutral), false);
+    public static readonly StyledProperty<bool>    IsNegativeProperty      = AvaloniaProperty.Register<TransactionRow, bool>(nameof(IsNegative), false);
 
     public string IconText       { get => GetValue(IconTextProperty);       set => SetValue(IconTextProperty, value); }
     public IBrush IconBackground { get => GetValue(IconBackgroundProperty); set => SetValue(IconBackgroundProperty, value); }
@@ -23,6 +24,7 @@ public partial class TransactionRow : UserControl
     public string Amount         { get => GetValue(AmountProperty);         set => SetValue(AmountProperty, value); }
     public bool   IsPositive     { get => GetValue(IsPositiveProperty);     set => SetValue(IsPositiveProperty, value); }
     public bool   IsNeutral      { get => GetValue(IsNeutralProperty);      set => SetValue(IsNeutralProperty, value); }
+    public bool   IsNegative     { get => GetValue(IsNegativeProperty);     set => SetValue(IsNegativeProperty, value); }
 
     public TransactionRow()
     {
