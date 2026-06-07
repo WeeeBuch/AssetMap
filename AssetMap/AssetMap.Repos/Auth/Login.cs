9cfa97a7b6b962be260d5f4bbd1c2084faa4caa9
KikiZC <kikizc22@gmail.com>
2026-06-07 15:02:38 +0200

    bit of login logic

---
+++ AssetMap/AssetMap.Repos/Auth/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetMap.Repos.Auth
+{
+    public static class Login
+    {
+        public static (bool, string) TryLogin(string username, string password)
+        {
+            bool loged = false;
+            string statusCode = "";
+
+#if DEBUG
+            if (username == "demo" && password == "demo")
+            {
+                loged = true;
+                statusCode = "OK";
+            }
+#endif
+
+
+
+
+
+            return (loged, statusCode);
+        }
+    }
+}
